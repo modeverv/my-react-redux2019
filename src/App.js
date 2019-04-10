d4fb87cae0bb91fdf362180bebe1227cf14fd723
modeverv <modeverv@gmail.com>
2019-04-10 13:59:55 +0900

    2019-04-10 13:59:55

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import React, { Component } from 'react';
+import React, {
+  Component
+} from 'react';
 
 class App extends Component {
   render() {
     const greeting = "Heeeey"
-    const dom = <input type="text" onClick={() => {alert(1)}}/>
-    //<h1 className="foo">{greeting}</h1>
-    return dom
+    const dom = <input type="text"
+      onChange={
+        () => {
+          console.log(this)
+        }
+      }
+    />
+    const ddm = <div>
+      <label htmlFor="bar" > bar </label>
+      <input type="text"
+          onChange={
+            () => {
+              console.log(this)
+            }
+          }
+      />
+    </div>
+      //<h1 className="foo">{greeting}</h1>
+      return ddm
+    }
   }
-}
 
-/*
+  /*
 class App extends Component {
-  render() {
-    return React.createElement(
-      "div",
-      null,
-      "Hello Wooooold"
-    );
-  } 
-}
-*/
-export default App;
+          render() {
+        return React.createElement(
+          "div",
+          null,
+          "Hello Wooooold"
+        );
+      } 
+    }
+    */
+    export default App;
