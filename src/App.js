caa73bb8349a3f2bf7ac1106c3320a9cbd4ff809
modeverv <modeverv@gmail.com>
2019-04-10 15:13:15 +0900

    learn state setState

--- src/App.js
+++ src/App.js
@@ -4,13 +4,28 @@ const App = () => (<Counter></Counter>)
 
 class Counter extends Component {
   constructor(props) {
-    super(props)
-    console.log(this.state)
-    this.state = {count: 0}
+    super(props);
+    console.log(this.state);
+    this.state = { count: 0 };
+  }
+  handlePlusButton = () => {
+    console.log("handlePlusButton");
+    this.setState({ count: ++this.state.count });
+  }
+
+  handleMinusButton = () => {
+    console.log("handleMinusButton");
+    this.setState({ count: --this.state.count });
   }
   render() {
-    console.log(this.state)
-    return (<div>counter: {this.state.count}</div>)
+    console.log(this.state);
+    return (
+      <React.Fragment>
+        <div>counter: {this.state.count}</div>
+        <button onClick={this.handlePlusButton}>+1</button>
+        <button onClick={this.handleMinusButton}>-1</button>
+      </React.Fragment>
+    );
   }
 }
 export default App;
