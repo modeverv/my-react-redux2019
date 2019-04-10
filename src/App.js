8b23d41dfafb95e697944bb8c3bb4c5cae9c3154
modeverv <modeverv@gmail.com>
2019-04-10 13:38:20 +0900

    2019-04-10 13:38:20

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import React, { Component } from 'react';
 
+
 class App extends Component {
   render() {
-    return (<div><h1>Hello world</h1></div>);
+    return <div><h1>Hello world</h1></div>;
   }
 }
 
+/*
+class App extends Component {
+  render() {
+    return React.createElement(
+      "div",
+      null,
+      "Hello Wooooold"
+    );
+  } 
+}
+*/
 export default App;
